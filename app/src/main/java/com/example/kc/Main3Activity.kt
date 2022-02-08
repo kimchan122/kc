15868f79a6dfc9efe930633d98116f4ddc5805ee
kimchan122 <chanchankim122@gmail.com>
2022-02-08 23:35:10 +0900

    액티비티1 화면 구성 및 액티비티2로 화면 전환 구현

---
+++ app/src/main/java/com/example/kc/Main3Activity.kt
@@ -0,0 +1,17 @@
+package com.example.kc
+
+import android.animation.ObjectAnimator
+import android.media.Image
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class Main3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_3main)
+    }
+}
--- app/src/main/java/com/example/kc/MainActivity.kt
+++ app/src/main/java/com/example/kc/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.kc
 
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var logo: ImageView=findViewById(R.id.Logo)
+        var logotext: TextView=findViewById(R.id.Logotext)
+        var name: TextView=findViewById(R.id.name)
+
+        val fadeOut = ObjectAnimator.ofFloat(logo, "alpha", 0f, 1f)
+        val fadeOut1 = ObjectAnimator.ofFloat(logotext, "alpha", 0f, 1f)
+        val fadeOut2 = ObjectAnimator.ofFloat(name, "alpha", 0f, 1f)
+
+        fadeOut.duration = 3000
+        fadeOut1.duration = 3000
+        fadeOut2.duration = 3000
+
+        fadeOut.start()
+        fadeOut1.start()
+        fadeOut2.start()
+
+        Handler().postDelayed({
+            val Intent = Intent(this, Main2Activity::class.java)
+            startActivity(Intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        },3000)
+
     }
 }
